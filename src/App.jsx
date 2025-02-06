e825b4641b10af1594cf14ef147a024e4c53785d
officialmusto <christianmustoj@gmail.com>
2025-02-06 01:43:07 -0500

    update font and color

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // css
 import './App.css'
 
+// assets
+import bannerBackground from './assets/videos/mustobs.mp4'
+
+
 // components
 import NavBar from './components/NavBar/NavBar'
 import About from './components/About/About'
@@ -14,6 +18,12 @@ function App() {
   return (
     <>
     <NavBar />
+    <div className="banner-div">
+  <video autoPlay loop muted playsInline>
+    <source src={bannerBackground} type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</div>
       <div className='all-el'>
         <div className='profile-and-about'>
             <ProfileCard />
