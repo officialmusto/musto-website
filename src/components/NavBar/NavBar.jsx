c35a3a2a4969faaf47b32001b14e17a4b4506c32
officialmusto <christianmustoj@gmail.com>
2025-02-06 02:15:50 -0500

    update navbar route

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,18 +8,22 @@ function NavBar() {
   return (
     <div id="nav-el">
       <nav className="nav">
-        <HashLink 
-        smooth to="/#home">
-          home
-        </HashLink>
-
+        <div className="home">
+          <HashLink 
+          smooth to="/#home">
+            home
+          </HashLink>
+        </div>
+        <div className="portfolio">
         <HashLink smooth to="/#portfolio">
           portfolio
         </HashLink>
-
+        </div>
+        <div className="resume">
         <HashLink smooth to="/#resume">
           resume
         </HashLink>
+        </div>
       </nav>
     </div>
   )
