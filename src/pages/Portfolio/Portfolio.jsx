8c54c2bde080678e02d3678eb940becec8f9db16
officialmusto <christianmustoj@gmail.com>
2025-02-07 21:07:34 -0500

    finally got the site back to how it use to be

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -6,14 +6,21 @@ import ProfileCard from '../../components/ProfileCard/ProfileCard'
 import ProjectList from '../../components/ProjectList/ProjectList'
 import Resume from '../../components/Resume/Resume'
 
+// assets
+import styles from './Portfolio.module.css'
+
 const Portfolio = () => {
   return (
-    <>
-      <ProfileCard />
-      <About />
-      <ProjectList />
-      <Resume />
-    </>
+    <div className={styles.allEl}>
+      <div className={styles.profileAndAbout}>
+        <ProfileCard />
+        <About />
+      </div>
+      <div className={styles.projectsAndResume}>
+        <ProjectList />
+        <Resume />
+      </div>
+    </div>
   )
 }
 
